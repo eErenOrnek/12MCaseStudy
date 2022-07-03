89e5f14ee5d08310cb733eb7cc91ff27eaa664b6
eErenOrnek <ornekeren7@gmail.com>
2022-07-03 22:52:00 +0300

    Implemented service layer for stored procedure

---
+++ 12MCaseStudy/Models/StockModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _12MCaseStudy.Models
+{
+    public class StockModel
+    {
+        public int SiraNo { get; set; }
+        public string IslemTur { get; set; }
+        public string EvrakNo { get; set; }
+        public string Tarih { get; set; }
+        public Decimal GirisMiktar { get; set; }
+        public Decimal CikisMiktar { get; set; }
+        public Decimal StokMiktar { get; set; }
+    }
+}
