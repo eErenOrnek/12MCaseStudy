2d1459482ce29df5ff43b249f742abcba5849104
eErenOrnek <ornekeren7@gmail.com>
2022-07-03 22:50:55 +0300

    Implemented business layer for STK table
    This service will be used for providing data to combobox in the form application.

---
+++ 12MCaseStudyBusinessLayer/Concrete/StkManager.cs
@@ -0,0 +1,26 @@
+﻿using _12MCaseStudyBusinessLayer.Abstract;
+using _12MCaseStudyDAL.Abstract;
+using _12MCaseStudyDAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12MCaseStudyBusinessLayer.Concrete
+{
+    public class StkManager : IStkService
+    {
+        private readonly IStkRepository _stkRepository;
+
+        public StkManager(IStkRepository stkRepository)
+        {
+            _stkRepository = stkRepository;
+        }
+
+        public List<Stk> GetAll()
+        {
+            return _stkRepository.GetAll();
+        }
+    }
+}
